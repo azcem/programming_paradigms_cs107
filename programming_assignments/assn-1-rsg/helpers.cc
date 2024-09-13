784362b90dc8418a6d554dc36adabe1320945ec9
Assem Ahmed <assem.a.r.askar@gmail.com>
2024-09-14 00:15:11 +0300

    add programming assignment 1 rsg

---
+++ programming_assignments/assn-1-rsg/helpers.cc
@@ -0,0 +1,46 @@
+#include <map>
+#include "definition.h"
+#include "production.h"
+using namespace std;
+
+//takes a ruleName string and grammar and returns a vector of its expansion
+vector<string> expandRule(map<string, Definition> grammar, string rule) {
+  Production root = grammar[rule].getRandomProduction();
+  vector<string> res;
+
+  //populate
+  res.insert(res.begin(), root.begin(), root.end());
+
+  for (vector<string>::iterator it = res.begin(); it != res.end(); it ++) {
+    if ((*it)[0] == '<') {
+      vector<string> expansion = expandRule(grammar, *it);
+      it = res.erase(it);
+      it = res.insert(it, expansion.begin(), expansion.end());
+    }
+  }
+  return res;
+}
+
+//print the vector of terminal words
+//case for period add newline
+//case for non comma add space
+//case for comma dont add space
+string printWords(vector<string> words) {
+  string res = "";
+  bool newLine = false;
+  for (auto it = words.begin(); it != words.end(); it++) {
+    if (it == words.begin()){ res = *it; continue;} 
+    if (*it == ",") res += ",";
+    else if (*it == ".") res += ".";
+    else {
+      if (newLine) res += *it;
+      else res += ' ' + *it;
+      newLine = false;
+    }
+    if (*((*it).rbegin()) == '.'){
+      res += '\n';
+      newLine = true;
+    } 
+  }
+  return res;
+}
---
+++ programming_assignments/assn-1-rsg/helpers.h
@@ -0,0 +1,2 @@
+vector<string> expandRule(map<string, Definition> grammar, string rule);
+string printWords(vector<string> words) ;
